aa8ced963ad5c63abc502220f886d5675d50af34
unknown <kamalaujla4vr@gmail.com>
2020-05-31 19:33:37 +0530

    Home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '',component: PostListComponent},
+  {path: '',component: HomeComponent},
   {path: 'create',component: PostCreateComponent,canActivate: [AuthGuard]},
   {path: 'edit/:postId',component: PostCreateComponent,canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
-  {path: 'signup',component:SignupComponent}
+  {path: 'signup',component:SignupComponent},
+  {path: 'feed',component:PostListComponent}
 ];
 
 @NgModule({
